f5b5273db7e326ed851c6e62031850ed17e1dfab
Thomas W <thomasdewitt@outlook.com>
2017-12-06 11:50:29 +0100

    headlight fixes
    cleanup, value tweak, comment

--- robotCar/Functions/headLight.c
+++ robotCar/Functions/headLight.c
@@ -1,25 +1,25 @@
 /*
- * headLight.c
- *
- * Created: 4-12-2017 12:13:23
- *  Author: Pittvandewitt
- */
+* headLight.c
+*
+* Created: 4-12-2017 12:13:23
+*  Author: Pittvandewitt
+*/
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 ISR(ADC_vect)
 {
-		uint16_t val = ADC;
+	uint16_t val = ADC;
 
-		if (val < 200)
-		{
-			PORTB=0b00000000;
-		}
-		else
-		{
-			PORTB=0b00000001;
-		}
+	if (val > 220)					// Sensitivity trigger value
+	{
+		PORTB=0b00000001;			// Port high
+	}
+	else
+	{
+		PORTB=0b00000000;			//Port low
+	}
 }
 
 void ADC_init(void)
@@ -31,9 +31,7 @@ void ADC_init(void)
 
 int headLight(void)
 {
-	DDRB |= (1<<DDB5) | (1<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1) | (1<<DDB0);
-	DDRB &= ~(1<<DDB7);
-	
+	DDRB |= (1<<DDB0);				// Initialize port db0
 	ADC_init();
 	
 	while (1)
